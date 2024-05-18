339c5963bb29f6e95a422b94fa6f29d1a6368411
Madirex <angel@madirex.com>
2024-05-18 09:49:49 +0200

    implemented add_closed_day and remove_closed_day

--- calendars/views.py
+++ calendars/views.py
@@ -6,6 +6,8 @@ from .serializers import CalendarModelSerializer, CalendarSerializer
 from .models import Calendar
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from datetime import datetime, date
 
 class CalendarViewSet(
     mixins.CreateModelMixin,
@@ -51,3 +53,44 @@ class CalendarViewSet(
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    @action(detail=True, methods=['post'], url_path='add-closed-day')
+    def add_closed_day(self, request, pk=None):
+        calendar = self.get_object()
+        closed_day_str = request.data.get('closed_day')
+        if not closed_day_str:
+            return Response({'detail': 'El día de cierre no se ha proporcionado.'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            closed_day = datetime.strptime(closed_day_str, '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'detail': 'Formato de fecha inválido.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if closed_day in calendar.closed_days:
+            return Response({'detail': 'El día de cierre ya está agregado en la lista.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if closed_day < date.today():
+            return Response({'detail': 'El día de cierre debe ser hoy o una fecha futura.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        calendar.closed_days.append(closed_day)
+        calendar.save()
+        return Response(CalendarModelSerializer(calendar).data)
+
+    @action(detail=True, methods=['delete'], url_path='remove-closed-day')
+    def remove_closed_day(self, request, pk=None):
+        calendar = self.get_object()
+        closed_day_str = request.data.get('closed_day')
+        if not closed_day_str:
+            return Response({'detail': 'Día de cierre no proporcionado.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            closed_day = datetime.strptime(closed_day_str, '%Y-%m-%d').date()
+        except ValueError:
+            return Response({'detail': 'Formato de fecha inválido. Use YYYY-MM-DD.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if closed_day not in calendar.closed_days:
+            return Response({'detail': 'Día de cierre no se encuentra agregado.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        calendar.closed_days.remove(closed_day)
+        calendar.save()
+        return Response(CalendarModelSerializer(calendar).data, status=status.HTTP_200_OK)
