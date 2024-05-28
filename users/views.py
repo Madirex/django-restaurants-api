33eb5bc8400072ef86c90aa2aa9fe2d34d88bc32
Madirex <angel@madirex.com>
2024-05-28 19:33:44 +0200

    fixed token

--- users/views.py
+++ users/views.py
@@ -88,8 +88,11 @@ class UserViewSet(viewsets.GenericViewSet):
         """User sign up."""
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        user = serializer.save()
-        data = UserModelSerializer(user).data
+        user, token = serializer.save()
+        data = {
+            'user': UserModelSerializer(user).data,
+            'access_token': token
+        }
         return Response(data, status=status.HTTP_201_CREATED)
 
     #TODO: arreglar e implementar email (problema dependencia con versión actual DJango)
