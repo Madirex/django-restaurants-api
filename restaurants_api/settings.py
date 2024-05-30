5e45335617c5a3915c54c4ff77117a3223536bd8
Madirex <angel@madirex.com>
2024-05-30 20:20:01 +0200

    refactor

--- restaurants_api/settings.py
+++ restaurants_api/settings.py
@@ -169,10 +169,10 @@ REST_FRAMEWORK = {
 }
 
 # Configuración del servidor SMTP
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-DEFAULT_FROM_EMAIL = os.environ['EMAIL_HOST_USER']
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_USE_TLS = True
+#EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+#EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+#DEFAULT_FROM_EMAIL = os.environ['EMAIL_HOST_USER']
